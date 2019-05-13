1954becb1af41f929a8223a617376376c0bb9594
PaaaulZ <skore.2o4@gmail.com>
2019-05-13 23:56:25 +0200

    Added routine to associate cities to province

--- IndieGuerraBot/IndieGuerraBot.py
+++ IndieGuerraBot/IndieGuerraBot.py
@@ -16,6 +16,21 @@ def loadConfig():
 
 config = loadConfig()
 
+def getLocationID(cityName):
+
+    mydb = mysql.connector.connect(host=config['dbhost'],user=config['dbuser'],passwd=config['dbpass'],database=config['dbase'])
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT id FROM locations WHERE city = '" + cityName + "'")
+
+    locationID = -1
+    for res in mycursor:
+        locationID = res[0]
+        break
+    
+    mydb.close()
+    
+    return locationID
+
 def getSpotifyAlbumID(artist,track,next = ''):
 
     if next is None or next == '':
@@ -34,7 +49,7 @@ def getSpotifyAlbumID(artist,track,next = ''):
 
         # Check if artist present
         for j in range(len(tracksJson['tracks']['items'][i]['artists'])):
-            if tracksJson['tracks']['items'][i]['artists'][j]['name'].lower().rstrip() == artist.lower().rstrip().encode('utf-8').decode('utf-8'):
+            if tracksJson['tracks']['items'][i]['artists'][j]['name'].lower().rstrip() == artist.lower().rstrip().encode('utf-8').decode('utf-8') or artist.lower().rstrip().encode('utf-8').decode('utf-8') in tracksJson['tracks']['items'][i]['name'] or artist.lower().rstrip().encode('utf-8').decode('utf-8') in tracksJson['tracks']['items'][i]['artists'][j]['name'].lower().rstrip():
                 return tracksJson['tracks']['items'][i]['album']['id']
 
         #if tracksJson['tracks']['next'] != '':
@@ -63,6 +78,22 @@ def getPlayCount(artist,track):
 
     return -3
 
+#def tmpAssocCity():
+
+#    mydb = mysql.connector.connect(host=config['dbhost'],user=config['dbuser'],passwd=config['dbpass'],database=config['dbase'])
+#    mycursor = mydb.cursor(buffered=True)
+#    mycursor2 = mydb.cursor(buffered=True)
+#    mycursor.execute("SELECT id,city FROM hits")
+
+#    for res in mycursor:
+#        locationID = getLocationID(res[1])
+#        mycursor2.execute("UPDATE hits SET locationID = " + str(locationID) + " WHERE id = '" + res[0] + "'")
+
+#    mydb.commit()
+#    mydb.close()
+
+#    return
+
 
 def updateScore():
 
@@ -79,33 +110,49 @@ def updateScore():
     mydb = mysql.connector.connect(host=config['dbhost'],user=config['dbuser'],passwd=config['dbpass'],database=config['dbase'])
 
     for fakeIndex in jsonMap:
-        artist = jsonMap[str(fakeIndex)][0]['artist']
-        title = jsonMap[str(fakeIndex)][0]['title']
-        city = jsonMap[str(fakeIndex)][0]['city']
+        artist = jsonMap[str(fakeIndex)][0]['artist'].rstrip()
+        title = jsonMap[str(fakeIndex)][0]['title'].rstrip()
+        city = jsonMap[str(fakeIndex)][0]['city'].rstrip()
         id = artist + "|" + title + "|" + city
         id = hashlib.md5(id.encode('utf-8')).hexdigest()
-
-        mycursor = mydb.cursor()
-        mycursor.execute("SELECT 1 FROM hits WHERE id = '" + id + "'")
-
-        for res in mycursor:
-            ignore = True
-
-        if ignore:
-            continue
+        locationID = getLocationID(city)
 
         playCount = getPlayCount(artist,title)
 
-        sql = "INSERT INTO hits (id, artist, title, city, playCount) VALUES (%s, %s, %s, %s, %s)"
-        val = (id, artist, title, city, playCount)
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT playCount FROM hits WHERE id = '" + id + "'")
+
+        for res in mycursor:
+            # Already exists, update
+            if playCount > res[0]:
+                # Got more playCount (test)
+                updateCursor = mydb.cursor()
+                updateCursor.execute("UPDATE hits SET playCount = " + str(playCount) + " WHERE id = '" + id + "'")
+                mydb.commit()
+            elif res[0] > 0 and playCount < 0:
+                # If I had a playCount but now I have not warn me.
+                print("WARNING: " + artist + " - " + title + " was " + str(res[0]) + " but now is " + playCount)
+
+            ignore = True
+
+            print("\t Updated " + title + " by " + artist)
+        if ignore:
+            continue
+
+
+        sql = "INSERT INTO hits (id, artist, title, city, playCount, locationID) VALUES (%s, %s, %s, %s, %s,%s)"
+        val = (id, artist, title, city, playCount, locationID)
         mycursor = mydb.cursor()
         
         mycursor.execute(sql, val)
         mydb.commit()
         
-        # DEBUG
-        print("\t Done " + title + " by " + artist)
+        print("\t Added " + title + " by " + artist)
+
+        mydb.close()
 
     return
 
-updateScore()
+
+tmpAssocCity()
+#updateScore()
